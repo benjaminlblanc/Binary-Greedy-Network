9d8b9841d8561e54c2aaaf8bcd78c84b07070f04
Benjamin Leblanc <66789896+benthewhite@users.noreply.github.com>
2022-07-08 11:17:48 -0400

    Update BGN.py

--- BGN.py
+++ BGN.py
@@ -1,3 +1,13 @@
+
+# Copyright 2022 Benjamin Leblanc
+
+# This file is part of Binary Greedy Network (BGN).
+
+# BGN is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
 import numpy as np
 from time import time
 from sklearn.linear_model import Lasso
